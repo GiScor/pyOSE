023a5ee7e0480986547cd3fc39fc5e6a5468068b
Gianluca Scorzoni <scorzonigianluca@gmail.com>
2026-08-11 15:45:27 +0200

    added main.py (TUI)

---
+++ main.py
@@ -0,0 +1,64 @@
+from classesraces import CharRace, CharClass
+from textual.binding import Binding
+from textual.app import App, ComposeResult
+from textual.widgets import Label, Static, Button, OptionList
+from textual.widget import Widget
+from textual.screen import Screen
+from textual import events, on
+from textual.containers import Horizontal, Vertical, VerticalScroll
+
+class CharacterCreation(Screen):
+    def compose(self) -> ComposeResult:
+        race_selector = SelectorDescriptor(options=['Dwarf', 'Elf'])
+        yield Static(" Create your advenutrer! ")
+        yield Button("Back",id='pop')
+        with Horizontal():
+            with Vertical(id="left-panel"):
+                # yield attribute_table
+                yield Static("attributes")
+            with Vertical(id="right-panel"):
+                yield race_selector
+                # yield class_selector
+        # yield name_input
+
+class SelectorDescriptor(Widget):
+    def __init__(self, options=[], descriptions='description here'):
+        super().__init__()
+        self.options = options
+        self.descriptions = descriptions
+    def compose(self) -> ComposeResult:
+        with Horizontal():
+            with Vertical(id="selector"):
+                    yield OptionList(*self.options)
+            with Vertical(id="description"):
+                yield Label(self.descriptions)
+    # @on(OptionList.OptionHighlighted)
+    # def description():
+
+
+class pyOSE(App):
+    BINDINGS = [
+        Binding("ctrl+x", "quit", "Quit"),
+    ]
+    SCREENS = {
+        'character-creation': CharacterCreation,
+    }
+
+    def compose(self):
+        yield Static(" Welcome to PyOSE! ")
+        yield Button("Character creation", id="character-creation")
+        yield Button("Combat", id="combat")
+
+    @on(Button.Pressed, "#character-creation, #combat-button")
+    def add_screen_to_stack(self, event: Button.Pressed) -> None:
+        self.push_screen(event.button.id)
+
+    @on(Button.Pressed, "#pop")
+    def pop_screen_from_stack(self, event: Button.Pressed) -> None:
+        self.pop_screen()
+
+
+
+if __name__ == '__main__':
+    OSE = pyOSE()
+    OSE.run()
